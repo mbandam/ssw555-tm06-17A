ee1ad5cede3516539baed6781fab08b175ac8ac9
Brendan Reis <brendanreis@gmail.com>
2017-06-06 13:46:20 -0400

    remove non-i/o code from 'with' block

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import Classes
+import pymongo
 
 def main():
     valid_tag_set = {"0 INDI", "1 NAME", "1 SEX", "1 BIRT", "1 DEAT", "1 FAMC", "1 FAMS", "0 FAM", "1 MARR", "1 HUSB",
@@ -6,35 +7,36 @@ def main():
 
     with open("test1.ged") as file:
         lines = [line.rstrip("\n") for line in file.readlines()]
-        id = 1
-        people = []
-        tags = []
-        for line in lines:
-            tokens = line.split(" ", maxsplit=2)
-            level = tokens[0]
-            tag = tokens[1]
 
-            args = ""
-            if len(tokens) > 2:
-                args = tokens[2]
+    id = 1
+    people = []
+    tags = []
 
-            if args == "INDI" or args == "FAM":
-                temp = args
-                args = tag
-                tag = temp
+    for line in lines:
+        tokens = line.split(" ", maxsplit=2)
+        level = tokens[0]
+        tag = tokens[1]
 
-            if (level + " " + tag) in valid_tag_set:
-                    tags.append(Classes.TagLine(level, tag, args))
-            else:
-                print("Ignoring '" + line + "' invalid format.")
-                continue
+        args = ""
+        if len(tokens) > 2:
+            args = tokens[2]
 
-            if (level + " " + tag) == "0 INDI" and len(tags) > 0:
-                for tag in tags:
-                    if Classes.TagLine.getTag(tag) == "NAME":
-                        people.append(Classes.Person(tags).getJson(id))
-                        id += 1
-                tags = []
+        if args == "INDI" or args == "FAM":
+            # Swap args and tag
+            args, tag = tag, args
+
+        if (level + " " + tag) in valid_tag_set:
+                tags.append(Classes.TagLine(level, tag, args))
+        else:
+            print("Ignoring '" + line + "' invalid format.")
+            continue
+
+        if (level + " " + tag) == "0 INDI" and len(tags) > 0:
+            for tag in tags:
+                if Classes.TagLine.getTag(tag) == "NAME":
+                    people.append(Classes.Person(tags).getJson(id))
+                    id += 1
+            tags = []
 
     #Here are 'Person' JSON objects we can persist in MongoDB :)
     for person in people:
